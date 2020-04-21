21b4fee667af0c8568c274d7603c9cb845ba9278
aquaphoenixZ <zacharia.hammami@gmail.com>
2020-04-21 16:56:48 -0400

    Added Order page user control

--- Cuddly Red Waffle app/Form1.Designer.cs
+++ Cuddly Red Waffle app/Form1.Designer.cs
@@ -32,9 +32,10 @@
             // 
             // mainPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "mainPage";
             this.Text = "Main Page";
             this.ResumeLayout(false);
