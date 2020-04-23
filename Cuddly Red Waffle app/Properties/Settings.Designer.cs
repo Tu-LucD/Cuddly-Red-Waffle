139024700aa48ce60c02537d09f4dfa81b4168f9
viki7133 <49701781+viki7133@users.noreply.github.com>
2020-04-23 00:20:27 -0400

    push #517

--- Cuddly Red Waffle app/Properties/Settings.Designer.cs
+++ Cuddly Red Waffle app/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Cuddly_Red_Waffle_app.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,8 @@ namespace Cuddly_Red_Waffle_app.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=TOMSINMUSTARD43;Initial Catalog=USANA_DB;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\USANA_DB.mdf;Integrated" +
+            " Security=True;Connect Timeout=30;User Instance=True")]
         public string USANA_DBConnectionString {
             get {
                 return ((string)(this["USANA_DBConnectionString"]));
