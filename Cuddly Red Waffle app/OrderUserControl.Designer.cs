0e2a01b6dd6ff76073ba5432134e185ef8095e09
viki7133 <49701781+viki7133@users.noreply.github.com>
2020-04-26 16:47:15 -0400

    something

--- Cuddly Red Waffle app/OrderUserControl.Designer.cs
+++ Cuddly Red Waffle app/OrderUserControl.Designer.cs
@@ -65,10 +65,11 @@
             this.orderListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.orderListBox.FormattingEnabled = true;
             this.orderListBox.HorizontalScrollbar = true;
-            this.orderListBox.ItemHeight = 20;
+            this.orderListBox.ItemHeight = 16;
             this.orderListBox.Location = new System.Drawing.Point(55, 53);
+            this.orderListBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.orderListBox.Name = "orderListBox";
-            this.orderListBox.Size = new System.Drawing.Size(190, 264);
+            this.orderListBox.Size = new System.Drawing.Size(190, 276);
             this.orderListBox.Sorted = true;
             this.orderListBox.TabIndex = 0;
             this.orderListBox.ValueMember = "OrderID";
@@ -91,7 +92,8 @@
             this.addOrderButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.addOrderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.addOrderButton.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.addOrderButton.Location = new System.Drawing.Point(55, 310);
+            this.addOrderButton.Location = new System.Drawing.Point(55, 329);
+            this.addOrderButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.addOrderButton.Name = "addOrderButton";
             this.addOrderButton.Size = new System.Drawing.Size(190, 30);
             this.addOrderButton.TabIndex = 1;
@@ -108,6 +110,7 @@
             this.myOrdersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.myOrdersLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.myOrdersLabel.Location = new System.Drawing.Point(55, 23);
+            this.myOrdersLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.myOrdersLabel.MinimumSize = new System.Drawing.Size(190, 30);
             this.myOrdersLabel.Name = "myOrdersLabel";
             this.myOrdersLabel.Size = new System.Drawing.Size(190, 30);
@@ -125,6 +128,7 @@
             this.orderDetailsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.orderDetailsLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.orderDetailsLabel.Location = new System.Drawing.Point(310, 23);
+            this.orderDetailsLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.orderDetailsLabel.MinimumSize = new System.Drawing.Size(240, 30);
             this.orderDetailsLabel.Name = "orderDetailsLabel";
             this.orderDetailsLabel.Size = new System.Drawing.Size(240, 30);
@@ -137,8 +141,9 @@
             this.orderInfoTab.Controls.Add(this.tabPage1);
             this.orderInfoTab.Controls.Add(this.tabPage2);
             this.orderInfoTab.Cursor = System.Windows.Forms.Cursors.Default;
-            this.orderInfoTab.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.orderInfoTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.orderInfoTab.Location = new System.Drawing.Point(310, 53);
+            this.orderInfoTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.orderInfoTab.Name = "orderInfoTab";
             this.orderInfoTab.SelectedIndex = 0;
             this.orderInfoTab.Size = new System.Drawing.Size(240, 306);
@@ -148,10 +153,11 @@
             // 
             this.tabPage1.Controls.Add(this.orderProductList);
             this.tabPage1.Controls.Add(this.totalPointsLabel);
-            this.tabPage1.Location = new System.Drawing.Point(4, 32);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(232, 270);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(232, 277);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Products";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -163,10 +169,11 @@
             this.orderProductList.DisplayMember = "Product In order";
             this.orderProductList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.orderProductList.FormattingEnabled = true;
-            this.orderProductList.ItemHeight = 20;
-            this.orderProductList.Location = new System.Drawing.Point(1, 16);
+            this.orderProductList.ItemHeight = 16;
+            this.orderProductList.Location = new System.Drawing.Point(1, 12);
+            this.orderProductList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.orderProductList.Name = "orderProductList";
-            this.orderProductList.Size = new System.Drawing.Size(229, 160);
+            this.orderProductList.Size = new System.Drawing.Size(172, 112);
             this.orderProductList.TabIndex = 0;
             this.orderProductList.ValueMember = "Product In order";
             // 
@@ -183,10 +190,11 @@
             this.totalPointsLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.totalPointsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.totalPointsLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.totalPointsLabel.Location = new System.Drawing.Point(-5, 239);
-            this.totalPointsLabel.MinimumSize = new System.Drawing.Size(240, 30);
+            this.totalPointsLabel.Location = new System.Drawing.Point(0, 247);
+            this.totalPointsLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.totalPointsLabel.MinimumSize = new System.Drawing.Size(232, 30);
             this.totalPointsLabel.Name = "totalPointsLabel";
-            this.totalPointsLabel.Size = new System.Drawing.Size(240, 30);
+            this.totalPointsLabel.Size = new System.Drawing.Size(232, 30);
             this.totalPointsLabel.TabIndex = 8;
             this.totalPointsLabel.Text = "Total points: 150";
             this.totalPointsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -202,10 +210,11 @@
             this.tabPage2.Controls.Add(this.orderPlacedDate);
             this.tabPage2.Controls.Add(this.label3);
             this.tabPage2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabPage2.Location = new System.Drawing.Point(4, 32);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(232, 270);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(232, 277);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Order Status";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -217,10 +226,11 @@
             this.estimatedDeliveryDate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.estimatedDeliveryDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.estimatedDeliveryDate.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.estimatedDeliveryDate.Location = new System.Drawing.Point(-4, 246);
-            this.estimatedDeliveryDate.MinimumSize = new System.Drawing.Size(240, 20);
+            this.estimatedDeliveryDate.Location = new System.Drawing.Point(1, 256);
+            this.estimatedDeliveryDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.estimatedDeliveryDate.MinimumSize = new System.Drawing.Size(232, 20);
             this.estimatedDeliveryDate.Name = "estimatedDeliveryDate";
-            this.estimatedDeliveryDate.Size = new System.Drawing.Size(240, 20);
+            this.estimatedDeliveryDate.Size = new System.Drawing.Size(232, 20);
             this.estimatedDeliveryDate.TabIndex = 11;
             this.estimatedDeliveryDate.Text = "May 1st 2020";
             this.estimatedDeliveryDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -232,10 +242,11 @@
             this.label10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label10.Location = new System.Drawing.Point(-3, 226);
-            this.label10.MinimumSize = new System.Drawing.Size(240, 20);
+            this.label10.Location = new System.Drawing.Point(-1, 236);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.MinimumSize = new System.Drawing.Size(232, 20);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(240, 20);
+            this.label10.Size = new System.Drawing.Size(232, 20);
             this.label10.TabIndex = 10;
             this.label10.Text = "Estimated Delivery Date";
             this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -245,9 +256,10 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(10, 150);
+            this.label7.Location = new System.Drawing.Point(10, 145);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(143, 20);
+            this.label7.Size = new System.Drawing.Size(119, 16);
             this.label7.TabIndex = 5;
             this.label7.Text = "Order Delivered";
             // 
@@ -255,9 +267,10 @@
             // 
             this.orderDeliveredDate.AutoSize = true;
             this.orderDeliveredDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.orderDeliveredDate.Location = new System.Drawing.Point(35, 170);
+            this.orderDeliveredDate.Location = new System.Drawing.Point(30, 165);
+            this.orderDeliveredDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.orderDeliveredDate.Name = "orderDeliveredDate";
-            this.orderDeliveredDate.Size = new System.Drawing.Size(109, 20);
+            this.orderDeliveredDate.Size = new System.Drawing.Size(85, 16);
             this.orderDeliveredDate.TabIndex = 4;
             this.orderDeliveredDate.Text = "May 1st 2020";
             // 
@@ -265,9 +278,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(10, 90);
+            this.label6.Location = new System.Drawing.Point(10, 85);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(101, 20);
+            this.label6.Size = new System.Drawing.Size(82, 16);
             this.label6.TabIndex = 3;
             this.label6.Text = "Order Sent";
             this.label6.Click += new System.EventHandler(this.label6_Click);
@@ -276,9 +290,10 @@
             // 
             this.orderSentDate.AutoSize = true;
             this.orderSentDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.orderSentDate.Location = new System.Drawing.Point(35, 110);
+            this.orderSentDate.Location = new System.Drawing.Point(30, 105);
+            this.orderSentDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.orderSentDate.Name = "orderSentDate";
-            this.orderSentDate.Size = new System.Drawing.Size(121, 20);
+            this.orderSentDate.Size = new System.Drawing.Size(93, 16);
             this.orderSentDate.TabIndex = 2;
             this.orderSentDate.Text = "April 27th 2020";
             // 
@@ -286,9 +301,10 @@
             // 
             this.orderPlacedDate.AutoSize = true;
             this.orderPlacedDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.orderPlacedDate.Location = new System.Drawing.Point(35, 50);
+            this.orderPlacedDate.Location = new System.Drawing.Point(30, 45);
+            this.orderPlacedDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.orderPlacedDate.Name = "orderPlacedDate";
-            this.orderPlacedDate.Size = new System.Drawing.Size(121, 20);
+            this.orderPlacedDate.Size = new System.Drawing.Size(93, 16);
             this.orderPlacedDate.TabIndex = 1;
             this.orderPlacedDate.Text = "April 20th 2020";
             this.orderPlacedDate.Click += new System.EventHandler(this.label4_Click);
@@ -297,9 +313,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(10, 30);
+            this.label3.Location = new System.Drawing.Point(10, 25);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(120, 20);
+            this.label3.Size = new System.Drawing.Size(100, 16);
             this.label3.TabIndex = 0;
             this.label3.Text = "Order Placed";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -310,7 +327,7 @@
             // 
             // OrderUserControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.orderInfoTab);
             this.Controls.Add(this.orderDetailsLabel);
@@ -318,6 +335,7 @@
             this.Controls.Add(this.addOrderButton);
             this.Controls.Add(this.orderListBox);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "OrderUserControl";
             this.Size = new System.Drawing.Size(602, 391);
             this.Load += new System.EventHandler(this.OrderPage_Load);
